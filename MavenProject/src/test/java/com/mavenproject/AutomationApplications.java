f5c06c3591096e89d4ebb12d521b5edebc36b6ba
RajaMaruthi <rekhaadbn@gmail.com>
2023-07-13 23:55:32 +0530

    This is my first commit

---
+++ MavenProject/src/test/java/com/mavenproject/AutomationApplications.java
@@ -0,0 +1,35 @@
+package com.mavenproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class AutomationApplications 
+{
+
+	WebDriver driver=null;
+	
+
+	@BeforeClass
+	public void launchBrowser()
+	{
+		
+			String strdriverpath=System.getProperty("user.dir")+"\\src\\test\\resources\\Drivers\\chromedriver.exe";
+			System.setProperty("webdriver.driver.chrome", strdriverpath);
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+	public void navigateUrl()
+	{
+		driver.get("https://www.guru99.com/software-testing.html");
+	}
+	
+	
+
+}
